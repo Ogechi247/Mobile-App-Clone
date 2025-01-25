0959fd909d3579f13f22c75b5bf2c4ad94cb49ea
Ogechukwukanma Ofili <ogechukwukanmaofili@gmail.com>
2025-01-25 16:39:38 -0500

    Create Color.kt

---
+++ Color.kt
@@ -0,0 +1,27 @@
+package com.example.whatsappclone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Whatsappthemecolor = Color(0xFFA7C7E7)
+val liteColor = Color(0xFFF5F5F5)
+
+val  textbackground =Color(0xFFdcf8c6)
+
+val  chatbackground =Color(0xFFece5dd)
+
+
+
+
+
+
+
+
+
+
